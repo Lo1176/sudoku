621b18c64691ff49874355caae30b7dcbe63d6a8
Laurent Binder <laurentbinder@gmail.com>
2020-11-13 19:08:37 +0100

    regions pass but need to be refactored

--- sudoku.rb
+++ sudoku.rb
@@ -17,9 +17,20 @@ class Sudoku
   end
 
   def regions
-    region_1r_1c = grid.map { |ary| ary.first(3) }.take(3).flatten
-    region_1r_3c = grid.map { |ary| ary.last(3) }.take(3).flatten
-    region_1r_2c = grid.map { |ary| ary.first(6).last(3) }.take(3).flattent
+    # regions on the first line
+    region_1r_1c = grid.map { |ary| ary.first(3) }.first(3).flatten
+    region_1r_2c = grid.map { |ary| ary.first(6).last(3) }.first(3).flatten
+    region_1r_3c = grid.map { |ary| ary.last(3) }.first(3).flatten
+    # regions on the middle line
+    # start with the forth_region
+    region_2r_1c = grid.map { |ary| ary.first(3) }.first(6).last(3).flatten
+    region_2r_2c = grid.map { |ary| ary.first(6).last(3) }.first(6).last(3).flatten
+    region_2r_3c = grid.map { |ary| ary.last(3) }.first(6).last(3).flatten
+    # regions on the last line
+    region_3r_1c = grid.map { |ary| ary.first(3) }.last(3).flatten
+    region_3r_2c = grid.map { |ary| ary.first(6).last(3) }.last(3).flatten
+    region_3r_3c = grid.map { |ary| ary.last(3) }.last(3).flatten
+    regions = [region_1r_1c, region_1r_2c, region_1r_3c, region_2r_1c, region_2r_2c, region_2r_3c, region_3r_1c, region_3r_2c, region_3r_3c]
   end
 
   def region_center
