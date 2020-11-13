1ae253cd1ffb843aba9bacb50ae2095fea802f27
Laurent Binder <laurentbinder@gmail.com>
2020-11-13 12:41:04 +0100

    wip

--- sudoku.rb
+++ sudoku.rb
@@ -17,8 +17,18 @@ class Sudoku
   end
 
   def regions
-    region_1_1 = grid.map { |e| e.first(3) }.take(3).flatten
-    region_3_1 = grid.map { |e| e.last(3) }.take(3).flatten
+    region_1r_1c = grid.map { |e| e.first(3) }.take(3).flatten
+    region_1r_3c = grid.map { |e| e.last(3) }.take(3).flatten
+    region_1r_2c = grid.reject { region_1r_1c && region_1r_3c }
+  end
 
+  def region_center
+    i = 3
+    region_1r_2c = []
+    until i == 5
+      region_1r_2c << grid.map { |e| e }
+      i += 1
+
+    end
   end
 end
